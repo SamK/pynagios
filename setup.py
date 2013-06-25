982feb76089da3a02199da909ee3cf272c2398c4
Sam <samyboy1+github@gmail.com>
2013-06-25 11:45:12 +0200

    new description of the setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='PyNagios',
       version='0.1',
-      description='Nagios utility',
+      description='Utility for Nagios scripts written in Python',
       author='Samuel Krieg',
       author_email='samuel.krieg+github@gmail.com',
       url='https://github.com/samyboy/pynagios',
