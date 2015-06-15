8818b69c9b168a7293df7450461fb8a4160b42b9
Samuel Krieg <samuel.krieg+github@gmail.com>
2015-06-15 11:57:31 +0200

    change setup.py for 0.2 version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='PyNagios',
-      version='0.1',
+      version='0.2',
       description='Utility for Nagios scripts written in Python',
       author='Samuel Krieg',
       author_email='samuel.krieg+github@gmail.com',
-      url='https://github.com/SamK/pynagios/tarball/0.1',
+      url='https://github.com/SamK/pynagios/tarball/0.2',
       packages=['pynagios'],
       keywords = ['nagios']
      )
